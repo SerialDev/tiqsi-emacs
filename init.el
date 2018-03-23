516bc22bfee352beb95d9292fe555652474d2ac2
Carlos Andres Mariscal Melgar <carlos.mariscal.melgar@gmail.com>
2018-03-23 23:19:11 +0200

    [+] Tiqsi barebones first commit!

---
+++ init.el
@@ -0,0 +1,13 @@
+;;; init.el --- Tiqsi Emacs base
+
+;;; Commentary: Bankrupcy highly inspired by Doom Emacs
+;; 
+
+;; These blocks are not part of GNU Emacs.
+;;
+;;; License: MIT
+
+
+
+
+;;; init.el ends here
---
+++ run.bat
@@ -0,0 +1,1 @@
+"C:\Users\andre\Desktop\SerialDev\emacs-bankrupt\mnt\NewEmacs\tools\emacs\emacs\bin\emacs.exe" -q -l init.el
