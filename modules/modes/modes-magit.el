f463ecbb09e025ffd19d0d798e9c2d5fef09d143
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2018-03-24 23:57:57 +0200

    [+] Magit git porcelain support

---
+++ modules/modes/modes-magit.el
@@ -0,0 +1,51 @@
+;;; modes-magit.el --- Tiqsi Magit configuration
+
+;;; Commentary:
+;; 
+
+(use-package magit
+  :straight t
+  :bind
+  (("C-x M-g" . magit-dispatch-popup))
+  :init
+  (setq magit-completing-read-function 'ivy-completing-read)
+  (global-magit-file-mode t))
+
+(use-package git-gutter-fringe
+  :straight t
+  :init
+  (global-git-gutter-mode)
+  (setq git-gutter-fr:side 'right-fringe))
+
+
+;--{Push upstream}--;
+
+(defun magit-push-arguments-maybe-upstream (magit-push-popup-fun &rest args)
+  "Enable --set-upstream switch if there isn't a current upstream."
+  (let ((magit-push-arguments
+         (if (magit-get-remote) magit-push-arguments
+           (cons "--set-upstream" magit-push-arguments))))
+    (apply magit-push-popup-fun args)))
+(advice-add 'magit-push-popup :around #'magit-push-arguments-maybe-upstream)
+
+;;Combined with ido completion this allows pushing a new branch with P P RET:
+
+;; NOTE: requires ido-completing-read+
+(package-install 'ido-completing-read+)
+(setq magit-completing-read-function #'magit-ido-completing-read)
+
+(defun sdev/magit-stage-and-commit()
+ (interactive)
+ (magit-stage)
+ (switch-to-buffer-other-window)
+ (magit-commit-popup 'c)) 
+
+
+;---{Keybindings}---;
+
+(define-key magit-mode-map "\C-cc" 'magit-commit)
+ ;(define-key python-mode-map (kbd "C-c c") 'sdev/magit-stage-and-commit)
+
+(provide 'modes-magit)
+
+;;; modes-magit.el ends here
