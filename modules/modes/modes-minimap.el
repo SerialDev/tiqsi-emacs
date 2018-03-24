87938de5c714f1d3f13d987cb1bb8192bc6549ac
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2018-03-24 23:56:24 +0200

    [+] minimap support

---
+++ modules/modes/modes-minimap.el
@@ -0,0 +1,14 @@
+;;; modes-minimap.el --- Tiqsi Minimap support
+
+;;; Commentary:
+;; 
+
+
+;---{Keybindings}---;
+
+(define-key global-map (kbd "C-#") 'minimap-mode)
+
+
+(provide 'modes-minimap)
+
+;;; modes-minimap.el ends here
