9b8a59e42a70c1a8da1d254bd22f8549f34ea17d
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2018-03-25 13:25:31 +0300

    [+] Avy mode support

---
+++ modules/modes/modes-avy.el
@@ -0,0 +1,30 @@
+;;; modes-avy.el --- Tiqsi avy configuration
+
+;;; Commentary:
+;; 
+
+(defun count-unique-visible-buffers (&optional frame)
+  "Count how many buffers are currently being shown.  Defaults to
+selected frame."
+  (length (cl-delete-duplicates (mapcar #'window-buffer (window-list frame)))))
+
+(defun sdev/jump-window (&optional frame)
+  (interactive)
+    (if
+	(> (count-unique-visible-buffers) 4)
+	(call-interactively #'ace-window)
+    (call-interactively #'other-window)))
+
+
+
+;---{keybindings}---;
+
+(global-set-key (kbd "M-w") 'sdev/jump-window)
+(global-set-key (kbd "C-c jj") 'avy-goto-word-or-subword-1)
+(global-set-key (kbd "C-c jw") 'ace-window)
+(global-set-key (kbd "C-c js") 'ace-swap-window)
+
+
+(provide 'modes-avy)
+
+;;; modes-avy.el ends here
