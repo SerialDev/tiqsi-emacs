190b6bf05f0fa36d47228db17e9ec26f8e6a81d9
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2018-03-25 13:24:48 +0300

    [+] Backus-Naur form grammars support

---
+++ modules/programming/programming-bnfc.el
@@ -0,0 +1,12 @@
+;;; programming-bnfc.el --- Tiqsi support for BNF Grammars
+
+;;; Commentary:
+;; 
+
+
+(add-to-list  'auto-mode-alist '("\\.tx\\'" . textx-mode))
+
+
+(provide 'programming-bnfc)
+
+;;; programming-bnfc.el ends here
