ae333a4c235d5b87920a182cfad713fe602c512e
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-09-10 16:51:59 +0300

    [+] Slime interaction Hydra

--- modules/programming/programming-lisp.el
+++ modules/programming/programming-lisp.el
@@ -32,10 +32,54 @@
 (add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
 (eval-after-load 'elisp-slime-nav '(diminish 'elisp-slime-nav-mode))
 
+(defhydra hydra-slime (:color pink :hint nil)
+  "
+^Slime useful commands^
+----------------------------------------------------------------------------------
+_c_: Compile defun  _x_: Export Symbol       _u_: Undefine defun               _<_: List Callers
+_p_: Start Slime   _a_: GOTO beg defun       _:_: Interactive eval             _>_: List Callees
+_S_: Eval defun    _Sa_: GOTO end defun      _t_: Trace                        _s_: eval last sexpr
+_c_: Call defun    _TAB_: Complete at point  _v_: Edit Value                   _r_: Eval in repl
+_q_: Close parens  _RET_: Expand-1           _I_: Inspect                      _ESC_: Exit
+_kl_: Load/Compile Buffer-File   _kc_: Compile Buffer-File (no load)  _l_: Load File
+"
+  ("c"   slime-compile-defun :color blue)
+  ("p"   slime :color blue)
+  ("S"   slime-eval-defun :color blue)
+  ("c"   slime-call-defun :color blue)
+  ("q"   slime-close-all-parens-in-sexp :color blue)
+  
+  ("x"   slime-export-symbol-at-point :color blue)
+  ("a"   slime-beginning-of-defun :color blue)
+  ("Sa"   slime-end-of-defun :color blue)
+  ("TAB"   slime-completion-at-point :color blue)
+  ("RET"   slime-expand-1 :color blue)
+
+  ("u"   slime-undefine-function :color blue)
+  (":"   slime-interactive-eval :color blue)
+  ("r"   slime-eval-region :color blue)
+  ("t"   slime-toggle-fancy-trace :color blue)
+  ("v"   slime-edit-value :color blue)
+  ("I"   slime-inspect :color blue)
+
+  ("<"   slime-list-callers :color blue)
+  (">"   slime-list-callees :color blue)
+  ("s"   slime-eval-last-expression :color blue)
+  ("r"   slime-eval-last-expression-in-repl :color blue)
+
+  ("l"   slime-load-file :color blue)
+  ("kl"   slime-compile-and-load-file :color blue)
+  ("kc"   slime-compile-file :color blue)
+  ("ESC" nil "Exit"))
+
 
 ;---{Keybindings}---;
 
+(define-key slime-mode-map (kbd "M-c") 'hydra-slime/body )
+
 (define-key emacs-lisp-mode-map (kbd "C-c C-s") 'eval-last-sexp)
+(define-key slime-mode-map (kbd "C-c C-s") 'slime-eval-last-expression)
+(define-key slime-mode-map (kbd "C-c C-p") 'slime)
 (define-key emacs-lisp-mode-map (kbd "C-.") 'elisp-slime-nav-describe-elisp-thing-at-point)
 (define-key emacs-lisp-mode-map (kbd "M-.") 'elisp-slime-nav-find-elisp-thing-at-point)
 
