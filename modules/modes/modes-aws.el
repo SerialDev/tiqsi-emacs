5d742f48bd9dbf71df55a6bdef91a95697096e3e
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-10-23 14:15:08 +0300

    [+] Support for aws-cloudformation

---
+++ modules/modes/modes-aws.el
@@ -0,0 +1,65 @@
+;;; modes.el --- Tiqsi aws support funcs  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018-  Andres Mariscal
+
+;; Author: Andres Mariscal <carlos.mariscal.melgar@gmail.com>
+
+;; URL: https://github.com/serialdev/tiqsi-emacs
+;; Keywords: lisp
+;; Version: 0
+;; Package-Requires: ((cl-lib "0.5") (emacs "24"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+;;
+
+(defun tiqsi-aws-insert-cloudformation-yaml ()
+  (interactive)
+  (insert
+   (format
+   "---
+AWSTemplateFormatVersion: %s
+
+Description:
+  String
+
+Metadata:
+  template metadata
+
+Parameters:
+  set of parameters
+
+Mappings:
+  set of mappings
+
+Conditions:
+  set of conditions
+
+Transform:
+  set of transforms
+
+Resources:
+  set of resources
+
+Outputs:
+  set of outputs
+" "'2010-09-09'")))
+
+
+
+(provide 'modes-aws)
+
+;;; modes-aws.el ends here
