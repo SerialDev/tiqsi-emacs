ad7094a38d966744ac29683f90fff874fa88dd77
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-11-28 10:37:50 +0200

    [+] Markdown mode support

--- modules/private/private-misc.el
+++ modules/private/private-misc.el
@@ -703,6 +703,14 @@ sEnter Extra information :")
 </details>
 "))
 
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 
 ;---{Keybindings}---;
 
