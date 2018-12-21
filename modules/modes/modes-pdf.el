1bd725fdb2e8dcc5bf5c33f95e11667d6d7c75bf
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-12-21 10:39:38 +0200

    [+] PDF grep support

---
+++ modules/modes/modes-pdf.el
@@ -0,0 +1,37 @@
+;;; modes-pdf.el --- Tiqsi pdf support  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018-  Andres Mariscal
+
+;; Author: Andres Mariscal <carlos.mariscal.melgar@gmail.com>
+
+;; URL: https://github.com/serialdev/tiqsi-emacs
+;; Keywords: lisp
+;; Version: 0
+;; Package-Requires: ((cl-lib "0.5") (emacs "24"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+;;
+
+(use-package pdfgrep
+  :straight t
+  :ensure t
+  )
+
+
+(provide 'modes-pdf)
+
+;;; modes-pdf.el ends here
