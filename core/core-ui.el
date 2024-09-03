8f5f696e8d02118982d7367c3b8b399067ddaa82
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2024-09-03 03:44:18 +0100

    [38;2;255;255;255m[-] Remove unnecessary key bindings from input-decode-map.[0m

--- core/core-ui.el
+++ core/core-ui.el
@@ -629,16 +629,7 @@ usage (pick-one test (1 2))
 
 ;; ------------------------------------------------------------------------- ;
 
-(define-key input-decode-map "\e[OA" [up])
-(define-key input-decode-map "\e[1;10B" [M-S-down])
-(define-key input-decode-map "\e[1;10C" [M-S-right])
-(define-key input-decode-map "\e[1;10D" [M-S-left])
 
-
-
-(define-key input-decode-map "\e[1;3A" [M-up])
-(define-key input-decode-map "\e[1;3B" [M-down])
-(define-key input-decode-map "\e[1;3C" [M-right])
 (define-key input-decode-map "\e[1;3D" [M-left])
 
 
