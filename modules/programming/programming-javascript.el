c7f4f71ed18054795bc60d73588abc330beb398b
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2024-09-06 12:03:57 +0100

    [38;2;255;255;255m[$] Prepend rjsx-mode and update configurations for JavaScript and JSX files.[0m

--- modules/programming/programming-javascript.el
+++ modules/programming/programming-javascript.el
@@ -52,11 +52,25 @@
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
+(straight-require 'rjsx-mode)
 
-(use-package jsx-mode
+(use-package rjsx-mode
   :ensure t
   :straight t
-  )
+  :mode ("\\.jsx?$" . rjsx-mode)
+  :config
+  (setq js-indent-level 2))
+
+(use-package rjsx-mode
+  :interpreter (("node" . rjsx-mode))
+  :mode (("\\.js?\\'" . rjsx-mode)
+          ("\\.jsx?\\'" . rjsx-mode))
+  :config (progn
+            (setq js2-basic-offset 2
+              js2-highlight-level 3
+              js2-bounce-indent-p t
+              js2-mode-show-strict-warnings nil)))
+
 
 (use-package nodejs-repl
   :ensure t
