ff9df3965687e317f9752d5aee3e6bb8d45f235f
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2024-10-05 21:42:05 +0100

    [$] Modified Straight.el bootstrap to check for existing directory and load bootstrap.el accordingly

--- init-lite.el
+++ init-lite.el
@@ -122,16 +122,22 @@ of an error, just add the package to a list of missing packages."
 
                                         ;{Bootstrap Straight};
 
-(let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
-       (bootstrap-version 3))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-      (url-retrieve-synchronously
-        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-        'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(when (file-directory-p (concat "~/volume/.emacs/" "straight"))
+  (progn (setq user-emacs-directory "~/volume/.emacs/")
+    (message "No radox bootstrapping needed")
+
+    (load (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el")))
+  (let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
+         (bootstrap-version 3))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+        (url-retrieve-synchronously
+          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+          'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage)))
+
 
 
 ;; Bootstrap use-package
